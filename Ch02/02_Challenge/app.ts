6184ab77f851695798dcbfaae9c89c3348ffa9ce
BaboaAtCity <Mohammad.Baboa@city.ac.uk>
2024-01-23 18:40:32 +0000

    challenge 2

--- Ch02/02_Challenge/app.ts
+++ Ch02/02_Challenge/app.ts
@@ -31,7 +31,7 @@ function addTodoItem(todo: string): toDoItem {
     return newTodo
 }
 
-function getNextId(items: Array<toDoItem>): number {
+function getNextId<T extends { id: number }>(items: T[]): number {
     return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
 }
 
